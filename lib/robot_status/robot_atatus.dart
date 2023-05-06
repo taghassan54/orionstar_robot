3ad973b76e36752d4a5a904e05d5b19867367067
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2023-05-06 12:31:51 +0400

    add states class

---
+++ lib/robot_status/robot_atatus.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+@immutable
+abstract class RobotState {}
+
+class Initial extends RobotState {}
+class NavigationState extends RobotState{}
+class SpeechState extends RobotState{}
+class SadState extends RobotState{}
+class SmileState extends RobotState{}
+class LikingState extends RobotState{}
+class BlinkLookUpState extends RobotState{}
+class TakePhotoState extends RobotState{}
