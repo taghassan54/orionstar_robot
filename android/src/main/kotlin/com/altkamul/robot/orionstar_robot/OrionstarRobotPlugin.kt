3a13726e11806df9667a58d959d63a7e3813fdde
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2023-07-31 16:45:48 +0400

    Update OrionstarRobotPlugin.kt

--- android/src/main/kotlin/com/altkamul/robot/orionstar_robot/OrionstarRobotPlugin.kt
+++ android/src/main/kotlin/com/altkamul/robot/orionstar_robot/OrionstarRobotPlugin.kt
@@ -1098,7 +1098,7 @@ class OrionstarRobotPlugin : FlutterPlugin, MethodCallHandler {
 
         if (person != null)
             RobotApi.getInstance()
-                .startFocusFollow(reqId++, person.id, 1000, 1F, mFocusListener)
+                .startFocusFollow(reqId++, person.id, 9000, 0.5F, mFocusListener)
     }
 
     private fun registerPerson(person: Person) {
